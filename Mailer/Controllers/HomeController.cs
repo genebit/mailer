45b398032b8c7df86129bcc3c2709adaf4e74dea
genebit <genebitara@gmail.com>
2023-02-25 23:38:46 +0800

    Implemented sending emails to google

---
+++ Mailer/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+using System.Web.Mvc;
+
+namespace Mailer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SendMail(Sender sender)
+        {
+            if (ModelState.IsValid)
+            {
+                MailMessage mail = new MailMessage
+                {
+                    From = new MailAddress("webdevcorp.samp@gmail.com", "WebDev Corp."),
+                    Subject = "[FEEDBACK] | Mailer.com",
+                    Body = sender.Message,
+                    IsBodyHtml = true
+                };
+
+                mail.To.Add(sender.Email);
+
+                SmtpClient smtp = new SmtpClient
+                {
+                    Port = 587,
+                    EnableSsl = true,
+                    UseDefaultCredentials = false,
+                    Host = "smtp.gmail.com",
+                    Credentials = new System.Net.NetworkCredential("webdevcorp.samp@gmail.com", "qvpfuhrwlofqxpsb")
+                };
+                smtp.Send(mail);
+
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(false, JsonRequestBehavior.DenyGet);
+        }
+    }
+
+    public class Sender
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [AllowHtml]
+        public string Message { get; set; }
+    }
+}
